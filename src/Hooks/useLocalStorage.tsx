7671df189da18168e8191ed17a3627459af0392a
MarcosRehtanz <marcos.rehtanz@gmail.com>
2023-10-26 04:25:13 -0300

    Feat(useLocalStorage): Add custom hook

---
+++ src/Hooks/useLocalStorage.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+
+export const useLocalStorage = (key: string) => {
+
+    const __localeStorage: any = window.localStorage.getItem(key)
+    const [localStorage, setLocaleStorage] = useState(JSON.parse(__localeStorage))
+
+    const addItem = (item: any) => {
+        const _localeStorage: any = window.localStorage.getItem(key)
+
+        if (_localeStorage) {
+            
+            const _newLocalStorage = [...JSON.parse(_localeStorage), item]
+            window.localStorage.setItem(key, JSON.stringify(_newLocalStorage))
+            setLocaleStorage(_newLocalStorage)
+
+        } else {
+
+            const _newLocalStorage: Array<any> = [item]
+            window.localStorage.setItem(key, JSON.stringify(_newLocalStorage))
+            setLocaleStorage(_newLocalStorage)
+
+        }
+
+    }
+
+    const removeItem = (item: any) => {
+        const _localeStorage: any = window.localStorage.getItem(key)
+        if (_localeStorage) {
+            const _newLocalStorage = [...JSON.parse(_localeStorage)].filter((element: any) => element !== item)
+            window.localStorage.setItem(key, JSON.stringify(_newLocalStorage))
+            setLocaleStorage(_newLocalStorage)
+        } else {
+            const _newLocalStorage: Array<any> = []
+            window.localStorage.setItem(key, JSON.stringify(_newLocalStorage))
+            setLocaleStorage(_newLocalStorage)
+        }
+    }
+
+    return { addItem, removeItem, localStorage }
+}
