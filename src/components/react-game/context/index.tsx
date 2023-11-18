e555502f51a2d3313fcf3dd8e814271423e1a751
MarcosRehtanz <marcos.rehtanz@gmail.com>
2023-11-18 01:18:13 -0300

    fix: fix speed

--- src/components/react-game/context/index.tsx
+++ src/components/react-game/context/index.tsx
@@ -11,11 +11,11 @@ const GameProvider = ({ children }) => {
     const [level, setLevel] = React.useState({ sizeBlock: 20, speed: 4 })
     const selectLevel = (l: number) => {
         switch (l) {
-            case 1: return { sizeBlock: 48, speed: 4 }
-            case 2: return { sizeBlock: 40, speed: 4 }
-            case 3: return { sizeBlock: 30, speed: 3 }
-            case 4: return { sizeBlock: 24, speed: 4 }
-            default: return { sizeBlock: 20, speed: 4 }
+            case 1: return { sizeBlock: 48, speed: 6 }
+            case 2: return { sizeBlock: 40, speed: 6 }
+            case 3: return { sizeBlock: 30, speed: 5 }
+            case 4: return { sizeBlock: 24, speed: 6 }
+            default: return { sizeBlock: 20, speed: 6 }
         }
     }
     const setter = (id: number) => setLevel(selectLevel(Math.ceil(id / 202)))
