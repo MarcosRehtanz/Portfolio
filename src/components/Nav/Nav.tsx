1013edd867e6ff0202de0c24faa7cbe384097296
MarcosRehtanz <marcos.rehtanz@gmail.com>
2023-12-02 16:20:48 -0300

    feat: change buttons

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -11,20 +11,51 @@ export const Nav: React.FC = (): JSX.Element => {
                 <span className="bg-clip-text text-transparent bg-gradient-to-r from-red-400 via-pink-500 to-purple-500"> {pokemon?.name}</span>
             </div>
 
-            <div className="flex w-fit justify-end content-center">
-                <a className="inline-block text-blue-300 no-underline hover:text-pink-500 hover:text-underline text-center h-12 p-2 md:h-16 md:p-4 transform hover:scale-125 duration-300 ease-in-out" href="https://www.linkedin.com/in/juan-marcos-mansilla">
-                    <svg className="fill-current h-11" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
-                        <path d="M20.5 2h-17A1.5 1.5 0 002 3.5v17A1.5 1.5 0 003.5 22h17a1.5 1.5 0 001.5-1.5v-17A1.5 1.5 0 0020.5 2zM8 19H5v-9h3zM6.5 8.25A1.75 1.75 0 118.3 6.5a1.78 1.78 0 01-1.8 1.75zM19 19h-3v-4.74c0-1.42-.6-1.93-1.38-1.93A1.74 1.74 0 0013 14.19a.66.66 0 000 .14V19h-3v-9h2.9v1.3a3.11 3.11 0 012.7-1.4c1.55 0 3.36.86 3.36 3.66z"></path>
-                    </svg>
+
+            <div className="p-1 pr-4 flex gap-2 w-fit justify-end content-center">
+
+                <a href="https://www.linkedin.com/in/juan-marcos-mansilla" target='_blank'>
+                    <button
+                        className="group flex justify-center items-center gap-2 group-hover:before:duration-500 group-hover:after:duration-500 after:duration-500 hover:border-neutral-900 duration-500 hover:duration-500 underline underline-offset-2 hover:underline hover:underline-offset-4 origin-left hover:decoration-2 hover:text-neutral-300 relative bg-neutral-900 px-2 py-2 border text-left p-3 text-gray-50 text-base font-bold rounded-lg overflow-hidden after:absolute after:z-10 after:w-12 after:h-12 after:content[''] after:bg-sky-900 after:-left-8 after:top-8 after:rounded-full after:blur-lg hover:after:animate-pulse"
+                    >
+                        <svg
+                            className="w-6 h-6 fill-neutral-50"
+                            height="100"
+                            preserveAspectRatio="xMidYMid meet"
+                            viewBox="0 0 24 24"
+                            width="100"
+                            x="0"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path d="M20.5 2h-17A1.5 1.5 0 002 3.5v17A1.5 1.5 0 003.5 22h17a1.5 1.5 0 001.5-1.5v-17A1.5 1.5 0 0020.5 2zM8 19H5v-9h3zM6.5 8.25A1.75 1.75 0 118.3 6.5a1.78 1.78 0 01-1.8 1.75zM19 19h-3v-4.74c0-1.42-.6-1.93-1.38-1.93A1.74 1.74 0 0013 14.19a.66.66 0 000 .14V19h-3v-9h2.9v1.3a3.11 3.11 0 012.7-1.4c1.55 0 3.36.86 3.36 3.66z"></path>
+                        </svg>
+                    </button>
                 </a>
-                <a
-                    className="inline-block text-blue-300 no-underline hover:text-pink-500 hover:text-underline text-center h-12 p-2 md:h-16 md:p-4 transform hover:scale-125 duration-300 ease-in-out"
-                    href="https://github.com/MarcosRehtanz"
-                >
-                    <svg className="fill-current h-16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+                <a href="https://github.com/MarcosRehtanz" target='_blank' >
+                    <button
+                        className="group flex justify-center items-center gap-2 group-hover:before:duration-500 group-hover:after:duration-500 after:duration-500 hover:border-neutral-900 duration-500 hover:duration-500 underline underline-offset-2 hover:underline hover:underline-offset-4 origin-left hover:decoration-2 hover:text-neutral-300 relative bg-neutral-900 px-2 py-2 border text-left p-3 text-gray-50 text-base font-bold rounded-lg overflow-hidden after:absolute after:z-10 after:w-12 after:h-12 after:content[''] after:bg-sky-900 after:-left-8 after:top-8 after:rounded-full after:blur-lg hover:after:animate-pulse"
+                    >
+                        <svg
+                            className="w-6 h-6 fill-neutral-50"
+                            height="100"
+                            preserveAspectRatio="xMidYMid meet"
+                            viewBox="0 0 100 100"
+                            width="100"
+                            x="0"
+                            xmlns="http://www.w3.org/2000/svg"
+                            y="0"
+                        >
+                            <path
+                                className="svg-fill-primary"
+                                d="M50,1.23A50,50,0,0,0,34.2,98.68c2.5.46,3.41-1.09,3.41-2.41s0-4.33-.07-8.5c-13.91,3-16.84-6.71-16.84-6.71-2.28-5.77-5.55-7.31-5.55-7.31-4.54-3.1.34-3,.34-3,5,.35,7.66,5.15,7.66,5.15C27.61,83.5,34.85,81.3,37.7,80a10.72,10.72,0,0,1,3.17-6.69C29.77,72.07,18.1,67.78,18.1,48.62A19.34,19.34,0,0,1,23.25,35.2c-.52-1.26-2.23-6.34.49-13.23,0,0,4.19-1.34,13.75,5.13a47.18,47.18,0,0,1,25,0C72.07,20.63,76.26,22,76.26,22c2.72,6.89,1,12,.49,13.23a19.28,19.28,0,0,1,5.14,13.42c0,19.21-11.69,23.44-22.83,24.67,1.8,1.55,3.4,4.6,3.4,9.26,0,6.69-.06,12.08-.06,13.72,0,1.34.9,2.89,3.44,2.4A50,50,0,0,0,50,1.23Z"
+                            ></path>
+                        </svg>
+                        {/* Github */}
+                    </button>
+                </a>
+                {/* <svg className="fill-current h-16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
                         <path d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"></path>
-                    </svg>
-                </a>
+                    </svg> */}
             </div>
         </div>
     </nav>)
