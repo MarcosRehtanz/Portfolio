16c967ec63683aadfbfc862c86273293a9153026
MarcosRehtanz <marcos.rehtanz@gmail.com>
2024-08-18 21:03:34 -0300

    implement modularization

---
+++ src/components/StackTools/StackCard/index.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { IconLink } from "../../IconLink/IconLink";
+import { Tool } from "../../../types";
+
+const ToolCard = ({ children }: { children: React.ReactNode }) => (
+  <div className="w-64 flex-shrink-0 flex flex-row flex-wrap gap-2 justify-center content-center">
+    {children}
+  </div>
+);
+
+interface StackCardProps {
+  title: string;
+  description: React.ReactNode;
+  toolList: Tool[];
+}
+
+export const StackCard = ({ title, description, toolList }: StackCardProps) => (
+  <section className="container md:pt-3 w-fit mx-auto poppins">
+    <div className="mx-5 md:mx-auto bg-[--color-1] rounded-xl shadow-md overflow-hidden md:max-w-3xl m-5">
+      <div className="w-full flex flex-wrap flex-col md:flex-row justify-center content-center items-center">
+        <div className="min-w-[200px] max-w-xs min-h-[200px] p-2 flex flex-col flex-wrap justify-start content-center">
+          <h1 className="w-full text-left tracking-wide text-3xl text-white font-bold">
+            {title}
+          </h1>
+          <div className="mt-2 px-3 pt-4 border-l-2 border-gray-500">
+            {description}
+          </div>
+          <br />
+        </div>
+        <ToolCard>
+          {toolList.map((tool, i) => {
+            return (
+              <IconLink
+                key={tool.name + i}
+                item={tool}
+                className={
+                  "h-[60px] min-w-[60px] flex justify-center items-center"
+                }
+              />
+            );
+          })}
+        </ToolCard>
+      </div>
+    </div>
+  </section>
+);
--- src/components/StackTools/StackTools.tsx
+++ src/components/StackTools/StackTools.tsx
@@ -1,137 +1,66 @@
 import React from "react";
 import { backend, frontend, tools } from "../../utils/stacks";
-import { IconLink } from "../IconLink/IconLink";
+import { StackCard } from "./StackCard";
 
-const StackCard = ({ children }: { children: React.ReactNode }) => (
-  <div className="w-64 flex-shrink-0 flex flex-row flex-wrap gap-2 justify-center content-center">
-    {children}
-  </div>
+const NameTool = ({ children }: { children: string }) => (
+  <b className="mx-auto text-green-400">{children}</b>
 );
 
 export const StackTools = () => {
-  const sectionTools =
-    "w-[320px] min-h-[300px] rounded-t  rounded-b-2xl bg-[--color-4] flex flex-row justify-center content-start flex-wrap gap-4 m-4";
-  const tittleCard =
-    "w-full bg-[--color-2] rounded-t text-center text-5xl font-normal font-mono";
-  const linkCard = "h-[60px] min-w-[60px] flex justify-center items-center";
-  const blockTech =
-    "w-64 flex-shrink-0 flex flex-row flex-wrap gap justify-center content-center";
-  const libraries = "text-[--color-2]";
-
   return (
     <div className="mx-auto md:pt-16">
       <hr />
       {/* <h1 className='w-full my-10 py-10 tracking-wide bg-[--color-1] uppercase text-center text-5xl text-[--color-2] font-bold'>Languajes and herramientas</h1> */}
 
       <div className="w-full h-auto justify-between flex flex-row flex-wrap gap-x-12">
-        <section className="container md:pt-3 w-fit mx-auto poppins">
-          <div className="mx-5 md:mx-auto bg-[--color-1] rounded-xl shadow-md overflow-hidden md:max-w-3xl m-5">
-            <div className="w-full flex flex-wrap flex-col md:flex-row justify-center content-center items-center">
-              <div className="min-w-[200px] max-w-xs min-h-[200px] p-2 flex flex-col flex-wrap justify-start content-center">
-                <h1 className="w-full text-left tracking-wide text-4xl text-white font-bold">
-                  Frontend
-                </h1>
-                <p className="mt-2 pl-3 pr-3 border-l-2 border-gray-500 indent-2 text-white">
-                  <br />
-                  He adquirido la sólida práctica de la modularización en{" "}
-                  <b className={libraries}>React</b>, gestión de estados
-                  globales mediante <b className={libraries}>Redux</b> y{" "}
-                  <b className={libraries}>Apollo Client</b>, y diseño de
-                  interfaz con librerías como{" "}
-                  <b className={libraries}>Tailwind</b> y{" "}
-                  <b className={libraries}>Bootstrap</b>, respaldado por{" "}
-                  <b className={libraries}>JavaScript</b> y{" "}
-                  <b className={libraries}>TypeScript</b> para funcionalidades
-                  lógicas.
-                </p>
-                <br />
-              </div>
-              <StackCard>
-                {frontend.map((tool, i) => {
-                  return (
-                    <IconLink
-                      key={tool.name + i}
-                      item={tool}
-                      className={linkCard}
-                    />
-                  );
-                })}
-              </StackCard>
-            </div>
-          </div>
-        </section>
+        <StackCard
+          title="Frontend"
+          description={
+            <p className="indent-2 text-white">
+              He adquirido la sólida práctica de la modularización en{" "}
+              <NameTool>React</NameTool>, gestión de estados globales mediante{" "}
+              <NameTool>Redux</NameTool> y <NameTool>Apollo Client</NameTool>, y
+              diseño de interfaz con librerías como{" "}
+              <NameTool>Tailwind</NameTool> y <NameTool>Bootstrap</NameTool>,
+              respaldado por <NameTool>JavaScript</NameTool> y{" "}
+              <NameTool>TypeScript</NameTool> para funcionalidades lógicas.
+            </p>
+          }
+          toolList={frontend}
+        />
 
-        <section className="container md:pt-3 w-fit mx-auto poppins">
-          <div className="mx-5 md:mx-auto bg-[--color-1] rounded-xl shadow-md overflow-hidden md:max-w-3xl m-5">
-            <div className="w-full flex flex-wrap flex-col md:flex-row justify-center content-center items-center">
-              <div className="min-w-[200px] max-w-xs min-h-[200px] p-2 flex flex-col flex-wrap justify-start content-center">
-                <h1 className="w-full text-left tracking-wide text-4xl text-white font-bold">
-                  Backend
-                </h1>
-                <p className="mt-2 pl-3 pr-3 border-l-2 border-gray-500 indent-2 text-white">
-                  <br />
-                  He adquirido la capacidad de desarrollar servidores sólidos en{" "}
-                  <b className={libraries}>Node.js</b> con{" "}
-                  <b className={libraries}>Express</b> y/o{" "}
-                  <b className={libraries}>Apollo GraphQL</b>, utilizando{" "}
-                  <b className={libraries}>Sequelize</b> como ORM para{" "}
-                  <b className={libraries}>PostgreSQL</b>. También soy
-                  competente en la preparación de pruebas con{" "}
-                  <b className={libraries}>Jest</b> y{" "}
-                  <b className={libraries}>Supertest.</b>
-                </p>
-                <br />
-              </div>
-              <StackCard>
-                {backend.map((tool, i) => {
-                  return (
-                    <IconLink
-                      key={tool.name + i}
-                      item={tool}
-                      className={linkCard}
-                    />
-                  );
-                })}
-              </StackCard>
-            </div>
-          </div>
-        </section>
+        <StackCard
+          title="Backend"
+          description={
+            <p className="indent-2 text-white">
+              He adquirido la capacidad de desarrollar servidores sólidos en{" "}
+              <NameTool>Node.js</NameTool> con <NameTool>Express</NameTool> y/o{" "}
+              <NameTool>Apollo GraphQL</NameTool>, utilizando{" "}
+              <NameTool>Sequelize</NameTool> como ORM para{" "}
+              <NameTool>PostgreSQL</NameTool>. También soy competente en la
+              preparación de pruebas con <NameTool>Jest</NameTool> y{" "}
+              <NameTool>Supertest.</NameTool>
+            </p>
+          }
+          toolList={backend}
+        />
 
-        <section className="container md:pt-3 w-fit mx-auto poppins">
-          <div className="mx-5 md:mx-auto bg-[--color-1] rounded-xl shadow-md overflow-hidden md:max-w-3xl m-5">
-            <div className="w-full flex flex-wrap flex-col md:flex-row justify-center content-center items-center">
-              <div className="min-w-[200px] max-w-xs min-h-[200px] p-2 flex flex-col flex-wrap justify-start content-center">
-                <h1 className="w-full text-left tracking-wide text-4xl text-white font-bold">
-                  Tools
-                </h1>
-                <p className="mt-2 pl-3 pr-3 border-l-2 border-gray-500 indent-2 text-white">
-                  <br />
-                  He trabajado con la terminal de{" "}
-                  <b className={libraries}>Bash</b> y utilizo{" "}
-                  <b className={libraries}>Git</b> para el control de versiones.
-                  Además, tengo experiencia en diseño gráfico utilizando{" "}
-                  <b className={libraries}>Adobe Illustrator</b> y{" "}
-                  <b className={libraries}>Photoshop</b>. También he
-                  desarrollado videojuegos con <b className={libraries}>C#</b>{" "}
-                  en <b className={libraries}>Unity</b> como parte de mi
-                  experiencia en desarrollo de juegos.
-                </p>
-                <br />
-              </div>
-              <StackCard>
-                {tools.map((tool, i) => {
-                  return (
-                    <IconLink
-                      key={tool.name + i}
-                      item={tool}
-                      className={linkCard}
-                    />
-                  );
-                })}
-              </StackCard>
-            </div>
-          </div>
-        </section>
+        <StackCard
+          title="Tools"
+          description={
+            <p className="indent-2 text-white">
+              He trabajado con la terminal de <NameTool>Bash</NameTool> y
+              utilizo <NameTool>Git</NameTool> para el control de versiones.
+              Además, tengo experiencia en diseño gráfico utilizando{" "}
+              <NameTool>Adobe Illustrator</NameTool> y{" "}
+              <NameTool>Photoshop</NameTool>. También he desarrollado
+              videojuegos con <NameTool>C#</NameTool> en{" "}
+              <NameTool>Unity</NameTool> como parte de mi experiencia en
+              desarrollo de juegos.
+            </p>
+          }
+          toolList={tools}
+        />
       </div>
     </div>
   );
